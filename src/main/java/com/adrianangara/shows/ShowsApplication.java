52f05a383e3c7ec20d2dc7e2b9563105abf84d22
AdrianSA <Adrian.angara@gmail.com>
2020-04-26 13:22:24 -0400

    First Commit

---
+++ src/main/java/com/adrianangara/shows/ShowsApplication.java
@@ -0,0 +1,13 @@
+package com.adrianangara.shows;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShowsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShowsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/adrianangara/shows/ShowsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adrianangara.shows;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShowsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

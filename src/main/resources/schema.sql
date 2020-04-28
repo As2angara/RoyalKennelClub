bf8cfa415cc163a7b91a6b2f329ad7f872cd9b44
AdrianSA <Adrian.angara@gmail.com>
2020-04-28 18:42:17 -0400

    Modified DB schema to create relational entities

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,9 +1,37 @@
+create table S_User {
+    id bigint PRIMARY KEY Auto_increment,
+    email VARCHAR(255),
+    firstname VARCHAR(255),
+    lastname VARCHAR(255),
+    password VARCHAR(255)
+}
+
 CREATE TABLE Contestant(
     id BIGINT  PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(255),
-    ownerName VARCHAR(255),
+    owner_id bigint,
     breed VARCHAR(255),
     dog_group VARCHAR(255),
     isMale bit,
     isSpecial bit
 );
+
+alter table Contestant
+    add foreign key (owner_id) references S_User(id);
+
+create table Show {
+    id bigint PRIMARY KEY Auto_increment,
+    show_date DATE,
+    location VARCHAR(255),
+    show_type VARCHAR(255)
+}
+
+create table Show_Contestant {
+    show_id bigint,
+    contestant_id bigint
+}
+
+alter table Show_Contestant
+    add foreign key (show_id) references Show(id);
+alter table Show_Contestant
+    add foreign key (contestant) references Contestant(id);

12a17ef7fad81cd5c342ff791cea0dab96533454
AdrianSA <Adrian.angara@gmail.com>
2020-04-29 15:15:34 -0400

    Completed READ operation of Show endpoint

--- src/main/java/com/adrianangara/shows/API/ShowController.java
+++ src/main/java/com/adrianangara/shows/API/ShowController.java
@@ -1,4 +1,35 @@
 package com.adrianangara.shows.API;
 
+import com.adrianangara.shows.DAO.Interfaces.ShowRepository;
+import com.adrianangara.shows.Models.Contestant;
+import com.adrianangara.shows.Models.Show;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(path="/show", produces="application/json")
+@CrossOrigin(origins="*")
 public class ShowController {
+
+    private final ShowRepository sr;
+
+    @Autowired
+    public ShowController(ShowRepository sr) {
+        this.sr = sr;
+    }
+
+    //READ Operations
+    @GetMapping
+    public Iterable<Show> getShowTable() {
+        return sr.getAll();
+    }
+
+    @GetMapping("/{id}")
+    public Show getShowByID(@PathVariable("id") int id) {
+        Show show = sr.getById(id);
+        return show;
+    }
+
+
+
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -7,3 +7,5 @@ insert into Contestant (name, owner_id, breed, dog_group, isMale, isSpecial)
 values ("Scruffy", 1, "Hound", "Terrier", 1, 0);
 
 --S_SHOW table
+insert into S_Show (show_date, location, show_type)
+values ('2020-11-06', "1495 Sandalwood Pkwy E, Brampton", "ALLBREED");

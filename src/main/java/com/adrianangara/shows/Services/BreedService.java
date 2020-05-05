4135b776696f2f35c7661c2105d1af2d1cf6f5ea
AdrianSA <Adrian.angara@gmail.com>
2020-05-05 01:18:59 -0400

    Added Breed and BreedService classes

---
+++ src/main/java/com/adrianangara/shows/Services/BreedService.java
@@ -0,0 +1,16 @@
+package com.adrianangara.shows.Services;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+public class BreedService {
+
+    private RestTemplate rest = new RestTemplate();
+
+
+    public Breed getBreeds() {
+        return rest.
+    }
+
+
+}
